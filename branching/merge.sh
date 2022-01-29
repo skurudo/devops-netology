076f1b08cbc1ac84a50a6f31d306b6c659e1b58f
pavel.galkin <pavel@galkin.su>
2022-01-29 03:30:27 +0300

    merge: @ instead *

--- branching/merge.sh
+++ branching/merge.sh
@@ -1,9 +1,8 @@
-# 2.3
 #!/bin/bash
 # display command line options
 
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
