41e565e0f6170455ba23e4e695c947163a4c5d71
Pavel Galkin <pavel@galkin.su>
2022-03-18 00:02:50 +0300

    4.3 start, 4.2 light change

--- DEVSYS-16/4.2. Использование Python для решения типовых DevOps задач/git-job2.py
+++ DEVSYS-16/4.2. Использование Python для решения типовых DevOps задач/git-job2.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+print("Введите полный путь к репозиторию(со знаком '/' в конце):")
+path = input()
 
 import os
 
-bash_command = ["cd ~/devops-netology", "git status"]
+os.chdir(path)
+bash_command = ["git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 #is_change = False
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
-        print(os.path.abspath(prepare_result))
+        print(path+prepare_result)
 #        break
